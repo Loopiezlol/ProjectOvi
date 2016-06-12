51cf200cc4d84209353b16ebf29bba55bdcb97c2
Catalin Buruiana <catalindotburuiana@yahoo.com>
2016-06-12 23:06:36 +0100

    update schema Order

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -3,9 +3,14 @@
 import mongoose from 'mongoose';
 
 var OrderSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: String
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  buyLater: Boolean,
+  orderDate: {type: Date, default: Date.now()},
+  items: [{
+    quantity: Number,
+    cake: {type: mongoose.Schema.Types.ObjectId, ref: 'Cake'}
+  }]
+  //map here
 });
 
 export default mongoose.model('Order', OrderSchema);
