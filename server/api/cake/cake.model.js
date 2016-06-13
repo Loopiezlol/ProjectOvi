6af75543fe4ed486643784dbb3194e28a2d65580
StefanTDS <tudosedotstefan@gmail.com>
2016-06-13 19:47:01 +0100

    commit test

--- server/api/cake/cake.model.js
+++ server/api/cake/cake.model.js
@@ -5,7 +5,9 @@ import mongoose from 'mongoose';
 var CakeSchema = new mongoose.Schema({
   name: String,
   info: String,
-  price: {type: String} 
+  price: {type: String},
+  imageBin: {data: Buffer, contentType: String},
+  imageUrl: String
 });
 
 export default mongoose.model('Cake', CakeSchema);
