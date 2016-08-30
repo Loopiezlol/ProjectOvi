51726bb72e8d981828106e7b4dc1b7364d369521
Catalin Buruiana <catalin@enki.com>
2016-08-30 01:05:20 +0100

    Fixes

--- server/config/seed.js
+++ server/config/seed.js
@@ -4,43 +4,8 @@
  */
 
 'use strict';
-import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
 
-Thing.find({}).remove()
-  .then(() => {
-    Thing.create({
-      name: 'Purple Kush',
-      info: 'Integration with popular tools such as Bower, Grunt, Babel, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, and Less.'
-    }, {
-      name: 'Sour Diesel',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
-    }, {
-      name: 'Jack Herer',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
-    }, {
-      name: 'Blue Dream',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
-    }, {
-      name: 'Kosher Kush',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
-    }, {
-      name: 'Granddaddy Purple',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
-    },{
-      name: 'Tampoane',
-      info: 'bune pentru femei'
-    });
-  });
 
 // User.find({}).remove()
 //   .then(() => {
