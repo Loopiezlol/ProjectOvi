d34c168af414f7bc1c989177d2844a042299679e
Andrei Calabangiu <andreidotcalabangiu@gmail.com>
2016-08-30 22:12:31 +0100

    update 0.01a

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,55 +7,6 @@
 import User from '../api/user/user.model';
 import Product from '../api/product/product.model';
 
-<<<<<<< HEAD
-Thing.find({}).remove()
-  .then(() => {
-    Thing.create({
-      name: 'Purple Kush',
-      info: 'Nuj ce sa zic,nu am incercat inca cred. Merge sigur!'
-    }, {
-      name: 'Sour Diesel',
-      info: 'Asta cica e bomba in Caliornia.Bagati tare!'
-    }, {
-      name: 'Jack Herer',
-      info: 'Ce pula mea e asta?'
-    }, {
-      name: 'Blue Dream',
-      info: 'Asta iti da vise umede!'
-    }, {
-      name: 'Kosher Kush',
-      info: 'Facuta de evrei pentru evrei.Aveti grija!'
-    }, {
-      name: 'Granddaddy Purple',
-      info: 'Cea mai smecherie de pe piata'
-    },{
-      name: 'Tampoane',
-      info: 'bune pentru femei'
-    },{
-      name: 'TUDOSE',
-      info: 'mai fa un pai'
-    }
-    )});
-  
-User.find({}).remove()
-  .then(() => {
-    User.create({
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test'
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
-    })
-    .then(() => {
-      console.log('finished populating users');
-    });
-  });
-=======
 
 // User.find({}).remove()
 //   .then(() => {
@@ -91,4 +42,3 @@ User.find({}).remove()
        console.log('finished populating users');
      });
    });
->>>>>>> master
